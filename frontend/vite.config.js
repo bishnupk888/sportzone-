7720c23e549658d9e2548397f73eeb547961bd4e
bishnupk888 <bishnupk888@gmail.com>
2024-07-26 04:19:21 +0530

    updated vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Adjust the chunk size warning limit if needed
+    chunkSizeWarningLimit: 1000, // Set to a higher value if necessary
+
+    rollupOptions: {
+      output: {
+        // Customize manual chunks
+        manualChunks: {
+          vendor: ['@zegocloud/zego-uikit-prebuilt'], // Add other large dependencies if needed
+        },
+      },
+    },
+  },
 })
