19e2c3475db51dbdbe2c9a6ab8f46709c3694694
bishnupk888 <bishnupk888@gmail.com>
2024-08-26 15:53:13 +0530

    updates on error pages

--- frontend/src/404.jsx
+++ frontend/src/404.jsx
@@ -9,7 +9,7 @@ const PageNotFound =()=>{
 return (
     <div className="bg-black absolute h-screen w-screen top-0 b-0 flex flex-col justify-center items-center space-y-4">
      <h1 className="text-redBorder text-8xl ">404!</h1>   
-    <h1 className="text-white text-4xl">Page Not Found</h1>
+    <h1 className="text-white text-4xl">Page Not Found </h1>
     <button className="text-textColor bg-buttonBgColor border border-redBorder px-4 py-2 rounded-[8px] hover:scale-105" onClick={handleClick }>back to home</button>
 
 
